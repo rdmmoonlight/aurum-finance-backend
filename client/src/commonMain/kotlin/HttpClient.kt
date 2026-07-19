a26f9a4cac7e74ed0172e6b1e36a927e7e12418c
Obscuron <ndopoer@gmail.com>
2026-07-19 09:37:41 +0700

    Delete client directory

--- client/src/commonMain/kotlin/HttpClient.kt
+++
@@ -1,9 +0,0 @@
-package com.aurumfinance
-
-import io.ktor.client.HttpClient
-import io.ktor.client.*
-import kotlinx.rpc.krpc.ktor.client.installKrpc
-
-val httpClient = HttpClient {
-    installKrpc()
-}
